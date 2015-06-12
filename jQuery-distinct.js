514bccf9e984e78bad867be52690edca7e7bad2c
Gergely Muka <muka.gergely@gmail.com>
2015-06-12 11:39:07 +0200

    Update jQuery-distinct.js

--- jQuery-distinct.js
+++ jQuery-distinct.js
@@ -10,11 +10,11 @@
  */
 
 $.extend({
-	distinct : function(anArray) {
-	   var result = [];
-	   $.each(anArray, function(i,v){
-		   if ($.inArray(v, result) == -1) result.push(v);
-	   });
-	   return result;
-	}
+  distinct : function(anArray) {
+    var result = [];
+    $.each(anArray, function(i,v){
+      if ($.inArray(v, result) == -1) result.push(v);
+    });
+    return result;
+  }
 });
