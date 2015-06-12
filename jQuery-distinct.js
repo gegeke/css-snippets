5819ec417f4ffc3272176a352aeb2dbbb18aa1fe
Gergely Muka <muka.gergely@gmail.com>
2015-06-12 11:38:19 +0200

    Update jQuery-distinct.js

--- jQuery-distinct.js
+++ jQuery-distinct.js
@@ -1,20 +1,20 @@
-	/*!
-	 * This function extends jQuery functionality to create an array of unique items from an
-	 * array of not (necessarily) unique items.
-	 * @anArray: function input, must be a JS array
-	 * @result: function output, it is a JS array
-	 * TODO: insert verification for array (handle case if @anArray is not a JS array)
-	 * TODO: make it function with JSON, jQuery objects, and enable it to output JSON (and / or jQuery objects)
-	 * this should be done with a second parameter (so calling the function would require the type of the output:
-	 * array, JSON, obj)
-	 */
-	
-	$.extend({
-		distinct : function(anArray) {
-		   var result = [];
-		   $.each(anArray, function(i,v){
-			   if ($.inArray(v, result) == -1) result.push(v);
-		   });
-		   return result;
-		}
-	});
+/*!
+ * This function extends jQuery functionality to create an array of unique items from an
+ * array of not (necessarily) unique items.
+ * @anArray: function input, must be a JS array
+ * @result: function output, it is a JS array
+ * TODO: insert verification for array (handle case if @anArray is not a JS array)
+ * TODO: make it function with JSON, jQuery objects, and enable it to output JSON (and / or jQuery objects)
+ * this should be done with a second parameter (so calling the function would require the type of the output:
+ * array, JSON, obj)
+ */
+
+$.extend({
+	distinct : function(anArray) {
+	   var result = [];
+	   $.each(anArray, function(i,v){
+		   if ($.inArray(v, result) == -1) result.push(v);
+	   });
+	   return result;
+	}
+});
